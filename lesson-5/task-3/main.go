1ec2c5573549c8c976f940d22f52a2f47630ca30
Tolan4ik <toooolaaaa@mail.ru>
2026-09-07 18:38:36 +0300

    feat: решить задание 3 и закрепить калькулятором calc

--- lesson-5/task-3/main.go
+++ lesson-5/task-3/main.go
@@ -6,7 +6,16 @@ func apply(n int, fn func(int) int) int {
 	return fn(n)
 }
 
+func calc(a, b int, op func(int, int) int) int {
+	return op(a, b)
+}
+
 func main() {
 	res := apply(5, func(x int) int { return x * x })
 	fmt.Println(res)
+
+	re := calc(10, 5, func(x, y int) int { return x + y })
+	umn := calc(10, 5, func(x, y int) int { return x * y })
+	fmt.Println(re)
+	fmt.Println(umn)
 }
