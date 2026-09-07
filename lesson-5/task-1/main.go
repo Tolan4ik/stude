74293f6e5f3e6d72599e2406a517e05979246c62
Tolan4ik <toooolaaaa@mail.ru>
2026-09-07 16:50:27 +0300

    feat: сделал ещё примеры таких задач

--- lesson-5/task-1/main.go
+++ lesson-5/task-1/main.go
@@ -9,13 +9,45 @@ func div(a, b int) (int, error) {
 	return a / b, nil
 }
 
+func area(a, b int) (int, error) {
+	if a <= 0 || b <= 0 {
+		return 0, fmt.Errorf("стороны должны быть больше нуля")
+	}
+	return a * b, nil
+}
+
+func withdraw(balance, amount int) (int, error) {
+	if amount <= 0 {
+		return 0, fmt.Errorf("сумма снятия должна быть больше нуля")
+	} else {
+		if amount > balance {
+			return 0, fmt.Errorf("Недостаточно средств")
+		}
+	}
+	return balance - amount, nil
+}
+
 func main() {
 
-	res, err := div(10, 2)
+	res, err := div(10, 0)
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println(res)
+	}
+
+	re, er := area(2, 4)
+	if er != nil {
+		fmt.Println(er)
+	} else {
+		fmt.Println(re)
+	}
+
+	resu, ero := withdraw(500, 100)
+	if ero != nil {
+		fmt.Println(ero)
+	} else {
+		fmt.Println(resu)
 	}
 
 }
